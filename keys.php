9e80a497e5faebd588d1073ccd5d09f057dbe192
Yuji <nobunobucarp@gmail.com>
2015-04-28 17:44:39 +0900

    Delete keys.php
    この情報があると勝手につぶやかれてしまう

--- keys.php
+++
@@ -1,7 +0,0 @@
-<?php
-$api_key = "mYPNid7gmSS4mW8w9mpbb59gz";
-$api_secret = "QqC5EMWTby4WEG1xZdXyLzpSOw5PsMpSdI4UuZ0x9ihK3zr6wy";
-$access_token = "3107730439-BHcV8OV6OUBcsGJlZbSCuegyQPnUBPtzVIFSyhY";	//アクセストークン
-$access_token_secret = "hrH2KJpkATGsSY376wysu6b0ZrofKDHozHjCSIwr4L0QM";	//アクセストークン・シークレット
-$callback_url = "http://nobuta.xyz/php/tweet.php";
-?>
